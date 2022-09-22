34de968028c0ed5296311e725c39527e01464da7
g3th <theinkandpen@yahoo.co.uk>
2022-09-22 02:53:56 +0100

    modified:   pass.kt

--- pass.kt
+++ pass.kt
@@ -1,22 +1,30 @@
 class Generator (var passLength: Int){
 		
-	var counter = 0	
+	var counter = 0
+	var repeatingLetter = false
 	var letters = "abcdefghijklmnopqrstuvwxyz"
+	var numbers = "0123456789"
+	var symbols = "#@[]!+$^&()>/"
 	var randomized: List<Char> = emptyList()
 	
-	fun jumble(){
-		while (counter < passLength){
-			var randomIndex = (0..(letters.length)).random()
-			randomized = randomized.plus(letters[randomIndex])
-			++ counter
+	fun randomPick():String {
+
+			while (counter < passLength){
+
+				var randomLetterNumberOrSymbol= listOf<Char>(letters[(1..(letters.length-1)).random()], numbers[(1..(numbers.length-1)).random()], symbols[(1..(symbols.length-1)).random()])
+
+				var pickFromRandomLetterNumberOrSymbol = listOf<Char>(randomLetterNumberOrSymbol[(0..2).random()])
+				randomized = randomized.plus(pickFromRandomLetterNumberOrSymbol)
+				++counter			
 			}
-		println(randomized)	
-		}
+		return randomized.joinToString(separator = "")
+	}
 }
 
-fun main(){
-	println("Enter jumbled word length: ")
+fun main() {
+
+	println("Enter Password length: ")
 	var jumbledWordLength = readLine()!!.toInt()
 	var genValue = Generator(jumbledWordLength)
-	genValue.jumble()
+	print(genValue.randomPick())
 }
