97c41441b874f2a2fbef1eb37e6a60da740b254a
g3th <theinkandpen@yahoo.co.uk>
2022-09-22 06:35:55 +0100

    modified:   pass.kt

--- pass.kt
+++ pass.kt
@@ -1,30 +1,57 @@
 class Generator (var passLength: Int){
 		
 	var counter = 0
-	var repeatingLetter = false
+	var repeatingLetter = 0
+	var repeatingNumber = 0
+	var capitalized = false
 	var letters = "abcdefghijklmnopqrstuvwxyz"
 	var numbers = "0123456789"
 	var symbols = "#@[]!+$^&()>/"
 	var randomized: List<Char> = emptyList()
 	
-	fun randomPick():String {
-
+	fun pickAValue():Char {	
+		var randomLetterNumberOrSymbol= listOf<Char>(letters[(1..(letters.length-1)).random()], numbers[(1..(numbers.length-1)).random()], symbols[(1..(symbols.length-1)).random()])
+		var pickFromRandomLetterNumberOrSymbol = randomLetterNumberOrSymbol[(0..2).random()]
+		return pickFromRandomLetterNumberOrSymbol
+		}
+		
+	fun randomPick():String {	
 			while (counter < passLength){
-
-				var randomLetterNumberOrSymbol= listOf<Char>(letters[(1..(letters.length-1)).random()], numbers[(1..(numbers.length-1)).random()], symbols[(1..(symbols.length-1)).random()])
-
-				var pickFromRandomLetterNumberOrSymbol = listOf<Char>(randomLetterNumberOrSymbol[(0..2).random()])
-				randomized = randomized.plus(pickFromRandomLetterNumberOrSymbol)
-				++counter			
-			}
+				var randomValue = pickAValue()				
+				if (randomValue.toString().toIntOrNull() == null){		// is letter
+					if (capitalized == false){
+						var randomValueToString:String = randomValue.toString().uppercase()
+						randomValue = randomValueToString.toInt().toChar()
+						print(randomValue)
+						randomized = randomized.plus(randomValue)
+						capitalized = true
+					}
+					if (repeatingLetter < 1){
+					randomized = randomized.plus(randomValue)
+					++counter
+					++ repeatingLetter
+					}
+					else {
+						repeatingLetter = 0	
+					}
+				} else {												// is number
+					if (repeatingNumber < 1){
+						randomized = randomized.plus(randomValue)
+						++counter
+						++ repeatingNumber 
+					} else {
+						repeatingNumber = 0
+						}
+					}	
+				}	
 		return randomized.joinToString(separator = "")
+		}
 	}
-}
 
 fun main() {
 
-	println("Enter Password length: ")
+	print("Enter Password length: ")
 	var jumbledWordLength = readLine()!!.toInt()
 	var genValue = Generator(jumbledWordLength)
-	print(genValue.randomPick())
+	println("\nYour generated password is ${genValue.randomPick()}")
 }
